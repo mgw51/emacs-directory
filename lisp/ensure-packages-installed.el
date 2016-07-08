5d0c64ec226caacd94c6c7dc187ccbffea0d599a
Matthew <matt@sambal.maple>
2016-07-08 09:56:26 -0400

    Conditionally update package list before attempting to install anything.

--- lisp/ensure-packages-installed.el
+++ lisp/ensure-packages-installed.el
@@ -6,11 +6,17 @@
 (defun ensure-packages-installed (package-list)
   "Ensure that all packages in PACKAGE-LIST are installed on the local system."
   (message "Checking for required packages on local system...")
-  (let ((missing-package-p nil))
+  (let ((missing-package-p nil)
+        (packages-updated-p nil))
+
     (dolist (p package-list)
       (when (not (package-installed-p p))
         (message "Install missing package \"%s\"" p)
         (setq missing-package-p t)
+        (unless packages-updated-p
+          (message "Update package list...")
+          (package-refresh-contents)
+          (setq packages-updated-p t))  ;  Perform this explicitly in case last operation in `package-refresh-contents' returns nil
         (package-install p)))
     (when (not missing-package-p)
       (message "All packages present"))))
