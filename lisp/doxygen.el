24d6ee910d1ebae6c5714455c3ae6ab97727fe0c
Matt <matthew.george.wood@gmail.com>
2017-01-25 00:53:19 -0500

    `doxygen-function' template generator handles prefix arguments for `\arg' tags
    lisp/doxygen.el
       1.  Created two helper functions to generate appropriate strings
           based on the number passed as a prefix argument.
       2.  Handle the case where there is no prefix argument passed by
           assigning a default value.

--- lisp/doxygen.el
+++ lisp/doxygen.el
@@ -48,9 +48,13 @@
 ;;;
 (defun doxygen-function (&optional number-args)
   "Insert doxygen function documentation template at point."
-  (interactive "*")  ; `*' for error on readonly buffer
+  (interactive "*P")  ; `*' for error on readonly buffer
+  (if (null number-args)
+      (setq number-args 0)  ; provice a default value if an argument was not passed
+    (if (consp number-args)
+        (setq number-args (car number-args))))
+  (beginning-of-line)
   (save-excursion
-    (beginning-of-line)
     (let ((start (point))
           end)
       (insert (concat
@@ -61,8 +65,7 @@
                "///\n"
                "///           \n"
                "///\n"
-               "/// \\arg     \n"
-               "///\n"
+               (build--arg-string number-args)
                "/// \\pre     \n"
                "///\n"
                "/// \\post    \n"
@@ -73,3 +76,18 @@
       (indent-region start end)))
   (forward-line)
   (end-of-line))
+
+(defun build--arg-string (num)
+  (when (and (not (null num))
+         (> num 0))
+    (concat
+     (build-string num "/// \\arg     \n")
+     "///\n")))
+
+(defun build-string (num arg)
+  "Build a string that repeats the ARGument NUMber times."
+  (if (eq num 0)
+      nil
+    (concat
+     arg
+     (build-string (1- num) arg))))
