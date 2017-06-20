aab22e6bcd150f0f6729322adbd93554a7f7c157
Matthew <matt@coriander.maple>
2017-06-19 21:56:43 -0400

    Summary: Reorganizing and streamlining `init.el'
    init.el:
      1. Combining core system setting into one section
      2. Combining Hooks and Hook Functions into one section
      3. Combining personal settings into one section
      4. Replace `setq' with `setf' and group multiple related
         assignments into same call to setf.
      5. Moved three calls that disable modes into a `dolist' call
         which iterates over the modes to disable.
      6. Do the same with the `progn' that held all the 'require's
         in the init file.
      7. Added a `tramp' setting for windows systems that sets
         the default program to PuTTy's 'plink'.

--- init.el
+++ init.el
@@ -1,50 +1,21 @@
-;;;; Matt's ~/.emacs
+;;; init.el -- My emacs init file.
+;;; Commentary:
+;;;     Some things.
+;;;
 
-(when (not (display-graphic-p))
-    (setq linum-format "%d "))      ; add space between line numbers and buffer text
-(setq-default indent-tabs-mode nil) ; indent with spaces only
-(global-linum-mode 1)               ; display line numbers in margin
-(column-number-mode 1)              ; display line and column number in status bar
-(setq-default c-basic-offset 2)     ; ensure that offset is two spaces and no more
-(setq make-backup-files nil)        ; do not make backup files (tilde files)
-(setq backup-directory-alist nil)
-(setq inhibit-splash-screen t)
-(progn                              ; Force tooltips to display in echo area
-  (tooltip-mode t)
-  (setq tooltip-use-echo-area t))
-(setq visible-bell t)               ; Flash mode-bar instead of ringing system bell
+;;; Code:
 
-;;; Turn off the following modes...
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-
-;;; ERC: Send IRC notices to my minibuffer
-(setq erc-echo-notices-in-minibuffer-flag t)
-
+;;;;;;;;;;;;;;;; Core System Settings
 ;;; Set load paths
-;; set top-level directory, and automatically add subdirectories
-(add-to-list 'load-path "~/.emacs.d/lisp/")
-(let ((default-directory "~/.emacs.d/lisp/"))
+(let ((default-directory "~/.emacs.d/lisp/"))  ; Shadow dynamic variable for next commands
   (normal-top-level-add-to-load-path '("."))
   (normal-top-level-add-subdirs-to-load-path))
 
-;;; Create settings for slime
-(progn
-  ;; Default lisp (not elisp) for use with slime
-  (setq inferior-lisp-program "/usr/bin/sbcl")
-  ;; slime-fancy loads pretty much everything
-  (setq slime-contribs '(slime-fancy))
-  (setq slime-lisp-implementations  ; We created and saved a core file for faster load times.
-        '((sbcl ("sbcl" "--core" "lisp/sbcl/sbcl.core-for-slime")))))
-
 ;;; Packages
-;;; Handle package archives and also ensure all required packages are installed.
 (progn
   (require 'package)     ; Pull in package.el
   (package-initialize)   ; Initialize it
-  (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                           ("marmalade" . "https://marmalade-repo.org/packages/")
+  (setf package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                            ("melpa" . "http://melpa.org/packages/")))
   (require 'ensure-packages-installed)  ; custom function that installs missing packages listed below
   (let ((package-list '(iy-go-to-char
@@ -55,27 +26,46 @@
                         magit)))
     (ensure-packages-installed package-list)))
 
-;;; Place all other Requires here
-(progn
-  (require 'php-mode)                ; this resides in my lisp folder
-  (progn
-    (setq py-install-directory "/emacs.d/lisp/python-mode.el-6.2.0")
-    (require 'python-mode))
-  (require 'select-comment-by-lang)  ; one of my functions
-  (require 'cpp-funcs)               ; my c/c++ helper functions
-  (require 'iy-go-to-char)
-  (and                               ; helm provides excellent incremental completion
-   (require 'helm)
-   (require 'helm-config))
-  (require 'key-chord)               ; map chord combinations to regular key-pairs pressed simultaneously
-  (require 'yasnippet)               ; snippet functionality
-  (require 'flycheck)                ; flycheck package for syntax checking on the fly
-  (require 'magit)
-  (require 'doxygen))                ; that's my own helper library
+;;; Load libraries and packages.
+(dolist (cool-thing '(php-mode               ; this resides in my lisp folder
+                      python-mode
+                      select-comment-by-lang ; one of my functions
+                      cpp-funcs              ; my c/c++ helper functions
+                      iy-go-to-char
+                      helm                   ; helm provides excellent incremental completion
+                      helm-config
+                      key-chord              ; map chord combinations to regular key-pairs pressed simultaneously
+                      yasnippet              ; snippet functionality
+                      flycheck               ; flycheck package for syntax checking on the fly
+                      magit
+                      slime)                 ; clisp mode and repl
+                    t)
+  (funcall 'require cool-thing))
 
+;;;;;;;;;;;;;;;; Personal Settings
+;;; General Settings
+(setq-default indent-tabs-mode nil) ; indent with spaces only
+(global-linum-mode 1)               ; display line numbers in margin
+(column-number-mode 1)              ; display line and column number in status bar
+(setq-default c-basic-offset 2)     ; ensure that offset is two spaces and no more
+(setf make-backup-files nil         ; do not make backup files (tilde files)
+      backup-directory-alist nil    ; we don't need a backup directory
+      inhibit-splash-screen t
+;      visible-bell t ; Flash mode-bar instead of ringing system bell
+      erc-echo-notices-in-minibuffer-flag t  ; ERC: Send IRC notices to minibuffer
+      inferior-lisp-program "/usr/bin/sbcl"  ; Slime: Default lisp
+      slime-contribs '(slime-fancy))  ; Slime: slime-fancy loads pretty much everything
+(dolist (mode '(tool-bar-mode menu-bar-mode scroll-bar-mode))
+  (funcall mode -1))
+(if (display-graphic-p)
+    (eval-after-load (load-theme 'abyss t))  ; GUI theme
+  (and
+   (setf linum-format "%d ")                 ; Terminal settings
+   (message "terminal")))
+(add-hook 'after-init-hook #'global-flycheck-mode)  ; Enable flycheck-mode (probably want to add this to hook functions
 
 ;;; Global Key Map and Bindings
-;;; Anything that should happen across all modes (more or less)
+;; Anything that should happen across all modes (more or less)
 ;; helm
 (and
  (global-set-key (kbd "M-x") #'helm-M-x)
@@ -84,10 +74,11 @@
 ;; key chord
 (and
  (key-chord-mode 1)
- (key-chord-define c++-mode-map "{}" "{\n\n}\C-p\t")
+ (key-chord-define c++-mode-map "pq" "{\n\n}\C-p\t")
  (key-chord-define-global "fj" #'iy-go-up-to-char)
  (key-chord-define-global "fk" #'iy-go-to-char-backward))
-;; General keybindings
+
+;;; General keybindings
 (and
  (fset 'sort-buffer-by-name  ; Create function cell and assign it to key chord
        "\M-2\M-x Buffer-menu-sort")
@@ -104,14 +95,9 @@
 (put 'narrow-to-page   'disabled nil)  ; Narrowing
 (put 'narrow-to-region 'disabled nil)  ;
 
-
-;; ;;; Evaluate these after load
-;; ;; Turn on 'which-func-mode' minor mode for these languages
-;; (eval-after-load "which-func"
-;;                  '(setq which-func-modes '(c++-mode c-mode python-mode php-mode lisp-mode emacs-lisp-mode)))
-
-
-;;; Macros and Hooks
+;;;;;;;;;;;;;;;; Hooks and Hook Functions
+;;; Hooks
+;;
 ;; Common C/C++ hooks. This hook will be run for many c-like languages,
 ;; but these keybindings may be overridden by defining local bindings in
 ;; a lower keymap for a given language. See 'https://www.masteringemacs.org/article/mastering-key-bindings-emacs'
@@ -122,10 +108,10 @@
 (add-hook 'emacs-lisp-mode-hook #'lisp-settings)
 (add-hook 'lisp-mode-hook #'lisp-settings)
 (add-hook 'sh-mode-hook #'bash-hook-func)
+(add-hook 'projectile-mode-hook #'projectile-hook-func)
 
-;;; Custom Hook functions
+;;; Custom Hook Functions
 (defun c-style-lang-hook-func ()
-  (electric-pair-mode)
   (superword-mode t)  ; treat underscore-separated words as a single word : true
   (subword-mode -1)   ; treat camelCase words as separate words: false     
   (c-set-offset 'case-label '+) ; indent case statements in a switch block
@@ -148,9 +134,9 @@
   ;; Add some keywords to to C++ mode
   (font-lock-add-keywords 'c++-mode
                           '(("nullptr" . font-lock-keyword-face)
-                            ("constexpr" . font-lock-keyword-face))))
+                            ("constexpr" . font-lock-keyword-face)))
   ; enable C++11 support
-  ;  (add-hook 'c++-mode-hook (lambda () (setq flycheck-gcc-language-standard "c++11"))))
+  (add-hook 'c++-mode-hook (lambda () (setf flycheck-gcc-language-standard "c++14"))))
 
 
 (defun lisp-settings ()
@@ -160,14 +146,16 @@ enable eldoc-mode."
   ;; these functions and others are already included in that mode.
   (eldoc-mode)
   (yas-reload-all)
+  (show-paren-mode t)
   (local-set-key (kbd "C-m") #'newline-and-indent)
   (local-set-key (kbd "C-c c") #'insert-triplet))
 
 (defun python-hook-func ()
   "Some call me... Tim."
   (setq-default indent-tabs-mode nil)  ; use spaces, not tabs
-  (setq tab-width 4)
+  (setf tab-width 4)
   (yas-reload-all)
+  (show-paren-mode t)
   (local-set-key (kbd "C-c c") #'insert-triplet)
   (local-set-key (kbd "C-c d") #'debug-comment)
   (local-set-key (kbd "C-c f") #'func-header))
@@ -176,9 +164,9 @@ enable eldoc-mode."
   "To be run when we open a bash shell script."
   (message "Welcome to shell script mode. Grrrrr!!")
   (yas-reload-all)
+  (show-paren-mode t)
   (local-set-key (kbd "C-c c") #'insert-triplet)
   (local-set-key (kbd "C-c d") #'debug-comment))
-
 
 (defun flycheck-hook-func()
   ; recommends use of gcc over clang on this page:  http://wiki.opencog.org/w/Flycheck_help#Configuration
@@ -186,30 +174,24 @@ enable eldoc-mode."
                 (append flycheck-disabled-checkers
                         '(c/c++-clang))))
 
-;; Color theme selection
-;;  This could be moved in to the auto-generated `custom-set-variables' section below,
-;;  however, since we are not using any of the new custom theme stuff in 24+, I moved
-;;  hoisted this out and put it here.
-;; (and
-;;  (color-theme-initialize)
-;;  (color-theme-clarity))
+(defun projectile-hook-func()
+  (local-set-key (kbd "C-c C-x f") #'helm-projectile-find-file-dwim))
 
-;;; Auto added by emacs24
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
-   ["#313131" "#D9A0A0" "#8CAC8C" "#FDECBC" "#99DDE0" "#E090C7" "#A0EDF0" "#DCDCCC"])
- '(background-color "#ffffff")
- '(background-mode light)
- '(cursor-color "#ffff00")
+   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
  '(custom-safe-themes
    (quote
-    ("14f0fbf6f7851bfa60bf1f30347003e2348bf7a1005570fd758133c87dafe08f" "4c9ba94db23a0a3dea88ee80f41d9478c151b07cb6640b33bfc38be7c2415cc4" "71ecffba18621354a1be303687f33b84788e13f40141580fa81e7840752d31bf" "108b3724e0d684027c713703f663358779cc6544075bc8fd16ae71470497304f" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" default)))
- '(fci-rule-color "#2D2D2D")
- '(foreground-color "#ffff00")
+    ("f9574c9ede3f64d57b3aa9b9cef621d54e2e503f4d75d8613cbcc4ca1c962c21" "b56ea05564419bcdd994e4c97ad9167d0d6abe535bd01ef5be986836c43389b7" "bcc6775934c9adf5f3bd1f428326ce0dcd34d743a92df48c128e6438b815b44f" "14f0fbf6f7851bfa60bf1f30347003e2348bf7a1005570fd758133c87dafe08f" default)))
+ '(fci-rule-color "#5E5E5E")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(vc-annotate-background "#202020")
  '(vc-annotate-color-map
    (quote
