d0b34790fbf2c5e5e2da32620e3f905724612742
Matt Wood <mwood@sensaphone.com>
2018-07-03 14:33:59 -0400

    Push mark when inserting curly braces.
    lisp/my-work-utils.el

--- lisp/my-work-utils.el
+++ lisp/my-work-utils.el
@@ -91,12 +91,14 @@ case we call `helm-projectile-find-file' instead."
 
 (defun insert-curly-braces (start end)
   "Enclose the region defined by START and END within curly braces.
+
 The braces will be the sole glyph placed on their respctive lines
 above and below the region.  If the region is not active, simply
 open three lines, place the braces each on their own line and place
 the cursor on the line between.  In both cases, indent according to
-current major-mode."
+current `major-mode'."
   (interactive "*r")
+  (push-mark)
   (if (region-active-p)
       (let ((text (buffer-substring start end)))
         (goto-char start)
