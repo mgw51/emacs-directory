a5a6dfa30755d5cecb527230460007e01ef6f058
Matthew <mwood@wasabi.maple.home>
2019-01-26 17:53:33 -0500

    Added some perl stuffs
    init.el:
      1.  Added a perl hook
      2.  Added a perl-mode alias
    
    mw-perl-related.el:
      1.  Perl script setup function to add boiler-plate to perls scripts.

---
+++ lisp/mw-perl-related.el
@@ -0,0 +1,23 @@
+;;; perl-related.el --- Elisp functions related to perl buffers.
+
+;;; Commentary:
+;;;    Provide some useful helpers.
+
+;;; Code:
+
+(defun mw-perl-setup(prefix)
+  "Add a shebang and some `use' statements to the top of a perl script.
+If the PREFIX key is used, use diagnostics instead of warnings at the top of the script."
+  (interactive "p")
+  (insert "#!/usr/bin/perl\n\n"
+          "use strict;\n"
+          (if (eq prefix 1)
+              "use warnings;\n"
+            "use diagnostics;\n")
+          "use v5.24;\n\n")
+  (save-buffer)
+  (set-file-modes (buffer-file-name) #o744))
+
+
+(provide 'mw-perl-related)
+;;; mw-perl-related.el ends here
