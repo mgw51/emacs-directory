0ce32dbffe0e0a62877261f3106492e58aaf19f0
Matthew Wood <mwood@sensaphone.com>
2020-08-11 00:46:09 -0400

    Allow byte-compiling of all packages when upgrading or installing.

--- init.el
+++ init.el
@@ -556,7 +556,8 @@ This was changed in version 27 to conform with XDG standards.")
       visible-bell t                ; Flash mode-bar instead of ringing system bell
       abbrev-file-name (concat *base-dir* "abbrev_defs")
       save-abbrevs 'silent          ; Abbrev-mode settings
-      compilation-scroll-output 'first-error)
+      compilation-scroll-output 'first-error
+      async-bytecomp-allowed-packages ’(all))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Global Key Map and Bindings
