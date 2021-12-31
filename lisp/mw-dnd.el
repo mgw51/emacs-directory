ec1fa351637a7045dfb6a3747e1593ccf466f608
Matthew Wood <matthew.george.wood@gmail.com>
2021-12-31 11:15:32 -0500

    Accept an initiative column and sort on it.
    1. Fix newline handling
    2. Require an initiative column from caller
    3. Sort on initiative column
    4. Remove debug

--- lisp/mw-dnd.el
+++ lisp/mw-dnd.el
@@ -5,8 +5,10 @@
 ;;;    DnD session.
 
 ;;; Code:
+(require 'org)
+
 (defun mw-dnd/make-battle-chart-from-region (start end)
-  "Create a battle chart from the region defined by START to END.
+  "Create a battle chart from the region defined from START to END.
 
 A battle chart is composed from two space-separated columns and
 as many rows as necessary which have been provided by the caller.
@@ -14,43 +16,38 @@ This function will insert two additional columns for each row,
 both of which begin at zero.
 
 Columns:
-  1st - Character name (provided by caller)
-  2nd - Starting HP    (provided by caller)
-  3rd - Damage sustained during combat
-  4th - Current HP
+  1st - Character name        (provided by caller)
+  2nd - Creature's initiative (provided by caller)
+  3rd - Starting HP           (provided by caller)
+  4th - Damage sustained during combat
+  5th - Current HP
 
 During each round of combat the DM should update total damage for
 each player character and NPC.  The table formula will handle
 calculating each character's current HP when updated."
   (interactive "*r")
-  (message "\nCreate a table")  ; [DEBUG]
   (save-excursion
     (let ((text (delete-and-extract-region start end)))
-      (message text)  ; [DEBUG]
       (insert (with-temp-buffer
                 (insert text)
                 (goto-char (point-min))
-                (insert "Char sHP Dam cHP\n")
-                (message (buffer-string))  ; [DEBUG]
+                (insert "Char Order sHP Dam cHP\n")
                 (let ((curr-point (point)))
                   (if (not (char-equal (char-before (point-max)) ?\n))
-                      (message "  Insert newline")  ; [DEBUG]
-                      (let ((curr (point)))
-                        (goto-char (point-max))
-                        (insert "\n")
-                        (goto-char curr)))
+                        (let ((curr (point)))
+                          (goto-char (point-max))
+                          (insert-char ?\n)
+                          (goto-char curr)))
                   (mw-dnd/add--columns)
-                  (message (buffer-string))  ; [DEBUG]
                   (org-table-convert-region (point-min) (point-max))
-                  (message (buffer-string))  ; [DEBUG]
                   (goto-char curr-point))
                 (org-table-insert-hline)
-                (message (buffer-string))  ; [DEBUG]
                 (goto-char (1- (point-max)))
                 (org-table-insert-hline)
-                (message (buffer-string))  ; [DEBUG]
+                (org-table-goto-column 2)
+                (org-table-sort-lines nil ?N) ; reverse numeric sort
                 (goto-char (point-max))
-                (insert "#+tblfm: $4=vsum($2 - $3)\n")
+                (insert "#+tblfm: $5=vsum($3 - $4)\n")
                 (forward-line -1)
                 (org-ctrl-c-ctrl-c)
                 (buffer-string))))))
@@ -68,9 +65,9 @@ Search forward and add two columns of '0' to each successive line
   "Alald."
   (let ((count 0))
     (while (not (eq (point) (point-max)))
-      (forward-char)
       (if (char-equal (char-after) ?\n)
-          (1+ count)))
+          (setq count (1+ count)))
+      (forward-char))
     count))
   
   (provide 'mw-dnd)
