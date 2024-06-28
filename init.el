7fc8cacae203fcf544b7ea23edb3aeb7eb702ba5
Matthew Wood <mwood@localhost.localdomain>
2024-06-28 11:49:39 -0400

    Purge helm from init.el and replace with vertico/consult/embark/marginalia
    init.el

--- init.el
+++ init.el
@@ -146,26 +146,187 @@
   :commands magit-status
   :bind ("C-c C-g" . #'magit-status))
 
-(use-package helm
-  :demand t
-  :blackout " Ƕ"
-  ;; We must explicitly bind these helm commands (globally)
-  :bind (("M-x" . #'helm-M-x)
-         ("C-x C-f" . #'helm-find-files)
-         ("C-x C-b" . #'helm-buffers-list)
-         ("M-s o" . #'helm-occur)
-         ("C-x r b" . #'helm-filtered-bookmarks))
+(use-package vertico
+  :init
+  (vertico-mode)
   :custom
-  (completion-styles '(flex))
+  (vertico-resize t))
+
+;; Example configuration for Consult
+(use-package consult
+  ;; Replace bindings. Lazily loaded due by `use-package'.
+  :bind (;; C-c bindings in `mode-specific-map'
+         ("C-c M-x" . consult-mode-command)
+         ("C-c h" . consult-history)
+         ("C-c k" . consult-kmacro)
+         ("C-c m" . consult-man)
+         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings in `ctl-x-map'
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; M-g bindings in `goto-map'
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s d" . consult-find)                  ;; Alternative: consult-fd
+         ("M-s c" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+
+  ;; Enable automatic preview at point in the *Completions* buffer. This is
+  ;; relevant when you use the default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Optionally configure the register formatting. This improves the register
+  ;; preview for `consult-register', `consult-register-load',
+  ;; `consult-register-store' and the Emacs built-ins.
+  (setq register-preview-delay 0.5
+        register-preview-function #'consult-register-format)
+
+  ;; Optionally tweak the register preview window.
+  ;; This adds thin lines, sorting and hides the mode line of the window.
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
   :config
-  ;; Other emacs commands can be swapped out for helm commands by
-  ;; enabling helm-mode
-  (helm-mode 1))
+
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key "M-.")
+  ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   ;; :preview-key "M-."
+   :preview-key '(:debounce 0.4 any))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; "C-+"
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
+
+  ;; By default `consult-project-function' uses `project-root' from project.el.
+  ;; Optionally configure a different project root function.
+  ;;;; 1. project.el (the default)
+  ;; (setq consult-project-function #'consult--default-project--function)
+  ;;;; 2. vc.el (vc-root-dir)
+  ;; (setq consult-project-function (lambda (_) (vc-root-dir)))
+  ;;;; 3. locate-dominating-file
+  ;; (setq consult-project-function (lambda (_) (locate-dominating-file "." ".git")))
+  ;;;; 4. projectile.el (projectile-project-root)
+  (autoload 'projectile-project-root "projectile")
+  (setq consult-project-function (lambda (_) (projectile-project-root)))
+  ;;;; 5. No project support
+  ;; (setq consult-project-function nil)
+)
+
+(use-package consult-lsp)
+(use-package consult-projectile)
+
+(use-package marginalia
+  ;; add annotations to the minibuffer entries
+  :bind (:map minibuffer-local-map
+              ("M-A" . marginalia-cycle))
+  ;; Adding the marginalia-mode call to the init section forces this package to be
+  ;; loaded immediately, not deferred.
+  :init
+  (marginalia-mode))
+
+(use-package embark
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  ;; Show the Embark target at point via Eldoc. You may adjust the
+  ;; Eldoc strategy, if you want to see the documentation from
+  ;; multiple providers. Beware that using this can be a little
+  ;; jarring since the message shown in the minibuffer can be more
+  ;; than one line, causing the modeline to move up and down:
+
+  ;; (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+
+  :config
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  ;; only need to install it, embark loads it after consult if found
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
+(use-package orderless
+  ;; fuzzy search in completion buffers
+  :custom
+  (completion-styles '(orderless basic))
+  (complete-catagory-overrides '((file (styles basic partial-completion)))))
 
 (use-package projectile
   :hook (prog-mode . projectile-mode)
   :bind (:map projectile-mode-map
-              ("C-c p" . projectile-command-map))
+              ("C-c p" . projectile-command-map)
+              ("C-c p h" . #'consult-projectile))
   :preface
   (defun mw-vlang-same-directory-src-and-test-files(dir)
     "vlang test files reside in the same directory as the corresponding source files."
@@ -175,14 +336,12 @@
     (concat (projectile-project-root) "test/unit_tests/"))
   :custom
   (projectile-mode-line-prefix " ¶")
-  (projectile-completion-system 'helm)
+  (projectile-completion-system 'auto) ; use `completing-read'
   (projectile-cache-file (concat (expand-file-name user-emacs-directory) "projectile/projectile.cache"))
   (projectile-enable-caching t)
   (projectile-enable-cmake-presets t)
   (projectile-create-missing-test-files t)
   :init (use-package ag
-          :after projectile)
-        (use-package helm-projectile
           :after projectile)
   :config
   (projectile-register-project-type 'vlang '("v.mod" "src" ".git" ".gitignore" ".gitattributes" ".editorconfig")
@@ -238,9 +397,6 @@
           (lsp-ui-sideline-show-code-actions t)
           (lsp-ui-peek-enable t)
           :after lsp-mode)
-        (use-package helm-lsp
-          :commands helm-lsp-workspace-symbol
-          :after (helm lsp-mode))
         (use-package lsp-treemacs
           :commands lsp-treemacs-error-list
           :after lsp-mode))
@@ -262,17 +418,16 @@
 
 (use-package cc-mode
   :ensure nil
-  :after lsp-mode
   :commands (c++-mode c-mode awk-mode java-mode)
   :chords (:map c++-mode-map
                 ("pq" . mw-insert-curly-braces))
   :bind (:map c-mode-base-map
               ("C-c f" . #'mw-func-header))
   :init
-  (add-hook 'c-mode-hook #'lsp-deferred)
   (add-hook 'c-mode-common-hook #'common-settings)
-  (add-hook 'c++-mode-hook #'lsp-deferred)
+  (add-hook 'c-mode-hook #'lsp-deferred)
   (add-hook 'c++-mode-hook #'c++-settings)
+  (add-hook 'c++-mode-hook #'lsp-deferred)
   :preface
   (defun common-settings()
     (superword-mode t) ; underscores
@@ -325,12 +480,12 @@
   :hook lsp-deferred
   :custom (lsp-v-vls-executable "vls_linux_x64"))
 
+(use-package elixir-mode)
+
 (use-package yaml-mode)
 (use-package toml-mode)
 (use-package cql-mode)
-(use-package systemd
-  :init (use-package helm-systemd
-          :after (helm systemd)))
+(use-package systemd)
 
 (use-package cmake-mode
   :hook cmake-font-lock-activate
@@ -435,7 +590,6 @@ recalculate any formulas that exist within it."
   :bind (:map restclient-mode-map
               ("C-c r s" . #'mw-restclient-get-session))
   :init
-  (use-package restclient-helm :after restclient)
   (use-package restclient-test :after restclient)
   (use-package company-restclient :after restclient))
 
